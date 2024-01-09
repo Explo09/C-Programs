ecaa2fcbae52f8aaa5334c65b7ee6887c51443e3
explo09 <dubeyswati907@gmail.com>
2024-01-09 22:34:09 +0530

    Fibonacci Series

---
+++ Series.c
@@ -0,0 +1,17 @@
+//  WAP to Display Fibonacci Sequence
+
+#include<stdio.h>
+int main() {
+    int i, n;
+    long long int a = 0, b = 1, c = 0;
+    printf("\nEnter the nth term: ");
+    scanf("%d", &n);
+    for (i = 0; i <n; i++)
+    {
+        printf("%lld ", c);
+        a = b; 
+        b = c; 
+        c = a + b;
+    }
+    return 0;
+}
